68294f53182ed983399301290be1d9d4d00dddfd
Your Name <you@example.com>
2023-09-06 23:27:32 +0500

    Дебаггер и гпт

--- MyCSharpProject/Program.cs
+++ MyCSharpProject/Program.cs
@@ -41,9 +41,9 @@ public class Program
         return result;
     }
     public static void dprint(s21_decimal val)
-{
-    Console.WriteLine($"{val.bits[0]}, {val.bits[1]}, {val.bits[2]}, {val.bits[3]}");
-}
+    {
+        Console.WriteLine($"{val.bits[0]}, {val.bits[1]}, {val.bits[2]}, {val.bits[3]}");
+    }
     public static s21_decimal s21add(s21_decimal val1, s21_decimal val2)
     {
         uint scale1 = (val1.bits[3] << 1) >> 17;
@@ -88,9 +88,7 @@ public class Program
 
         return result;
     }
-
-    // Console.WriteLine("Целая часть числа: " + multiply(ref sum));
-        public static s21_decimal s21mul(s21_decimal val1, s21_decimal val2)
+    public static s21_decimal s21mul(s21_decimal val1, s21_decimal val2)
     {
         uint scale1 = (val1.bits[3] << 1) >> 17;
         uint scale2 = (val2.bits[3] << 1) >> 17;
@@ -99,7 +97,30 @@ public class Program
         decimal dec2 = (decimal)val2.bits[0] + (decimal)val2.bits[1] * 4294967296m + (decimal)val2.bits[2] * 18446744073709551616m;
         if((val2.bits[3] >> 31) == 1) dec2 *= -1;
         decimal sum = (dec1 / decimalPow(10, scale1)) * (dec2 / decimalPow(10, scale2));
-        Console.WriteLine("Целая часть числа: " + sum);
+        uint scale3 = (uint)(BitConverter.GetBytes(decimal.GetBits(sum)[3])[2]) << 16;
+        multiplyMul(ref sum, (scale3 >> 16));
+        decimal num1 = Math.Abs(sum) / 18446744073709551616m;
+        decimal temp1 = Math.Abs(sum) - decimal.Truncate(num1) * 18446744073709551616m;
+        decimal num2 = temp1 / 4294967296m;
+        decimal num3 = temp1 - decimal.Truncate(num2) * 4294967296m;
+        uint n1 = (uint)decimal.Truncate(num1);
+        uint n2 = (uint)decimal.Truncate(num2);
+        uint n3 = (uint)decimal.Truncate(num3);
+        s21_decimal result = new s21_decimal(new uint[] { n3, n2, n1, scale3 });
+        if(sum < 0) result.bits[3] |= 0x80000000;
+
+        return result;
+    }
+
+    public static s21_decimal s21mul(s21_decimal val1, s21_decimal val2)
+    {
+        uint scale1 = (val1.bits[3] << 1) >> 17;
+        uint scale2 = (val2.bits[3] << 1) >> 17;
+        decimal dec1 = (decimal)val1.bits[0] + (decimal)val1.bits[1] * 4294967296m + (decimal)val1.bits[2] * 18446744073709551616m;
+        if((val1.bits[3] >> 31) == 1) dec1 *= -1;
+        decimal dec2 = (decimal)val2.bits[0] + (decimal)val2.bits[1] * 4294967296m + (decimal)val2.bits[2] * 18446744073709551616m;
+        if((val2.bits[3] >> 31) == 1) dec2 *= -1;
+        decimal sum = (dec1 / decimalPow(10, scale1)) * (dec2 / decimalPow(10, scale2));
         uint scale3 = (uint)(BitConverter.GetBytes(decimal.GetBits(sum)[3])[2]) << 16;
         multiplyMul(ref sum, (scale3 >> 16));
         decimal num1 = Math.Abs(sum) / 18446744073709551616m;
@@ -119,8 +140,8 @@ public class Program
     {
         uint e1 = 65536;
         uint m1 = 0x80000000;
-        s21_decimal decimalValue1 = new s21_decimal(new uint[] {212331, 32333, 2513221, e1*6});
-        s21_decimal decimalValue2 = new s21_decimal(new uint[] {145312, 0, 0, e1*2});
+        s21_decimal decimalValue1 = new s21_decimal(new uint[] {212331, 32333, 1332, e1*6});
+        s21_decimal decimalValue2 = new s21_decimal(new uint[] {14312, 0, 0, e1*2});
         s21_decimal res = s21mul(decimalValue1, decimalValue2);
         dprint(res);
         m1--;
--- src/main.c
+++ src/main.c
@@ -1,10 +1,10 @@
-#include "src/s21_decimal.h"
+#include "s21_decimal.h"
 
 void dprint(s21_decimal val) {printf("%u, %u, %u, %u\n", val.bits[0], val.bits[1], val.bits[2], val.bits[3]);}
 
 int main() {
-    s21_decimal n1 = {{212331, 32333, 2513221, e1*6}};
-    s21_decimal n2 = {{145312, 0, 0, e1*2}};
+    s21_decimal n1 = {{212331, 32333, 1332, e1*6}};
+    s21_decimal n2 = {{14312, 0, 0, e1*2}};
     // s21_set_sign(&n2, 1);
     s21_decimal res = {{0}};
     s21_mul(n1, n2, &res);
